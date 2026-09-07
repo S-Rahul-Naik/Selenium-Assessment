43c201ff3bef2e30d081660e08f5cc7f55b2e1af
S-Rahul-Naik <23rahul54@gmail.com>
2026-09-08 00:42:35 +0530

    done

--- day1testcase1.java
+++ day1testcase1.java
@@ -1,15 +1,6 @@
 package Assessment;
 
-// Day 1: Assessment
-
-// TestCase-1
-
-// 1. Launch SauceDemo.
-// 2. Login using the valid credentials provided on the application.
-// 3. On the Products page, use JavaScript Executor to scroll to the bottom of the page.
-// 4. Take a screenshot of the page after scrolling.
-// 5. Save the screenshot with a meaningful name such as products-page.png.
-
+// Day 1 assessment: log in to SauceDemo, scroll to the bottom, and take a screenshot.
 
 import java.io.File;
 import java.io.IOException;
@@ -27,11 +18,14 @@ import org.openqa.selenium.io.FileHandler;
 
 public class day1testcase1 {
 	public static void main(String[] args) throws InterruptedException, IOException {
+		// Open Chrome and prepare the browser for the test.
 		WebDriver d = new ChromeDriver();
 		d.manage().window().maximize();
 		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		d.get("https://www.saucedemo.com/");
 		Thread.sleep(2000);
+
+		// Enter the valid SauceDemo login details.
 		WebElement username = d.findElement(By.xpath("//input[@id='user-name']"));
 		username.sendKeys("standard_user");
 		Thread.sleep(2000);
@@ -41,14 +35,20 @@ public class day1testcase1 {
 		WebElement loginButton = d.findElement(By.xpath("//input[@id='login-button']"));
 		loginButton.click();
 		Thread.sleep(3000);	
+
+		// Scroll to the footer so the bottom of the Products page is visible.
 		WebElement bottomEle = d.findElement(By.xpath("//div[@data-test='footer-copy']"));
 		JavascriptExecutor js = (JavascriptExecutor)d;
 		js.executeScript("arguments[0].scrollIntoView(false)",bottomEle);
 		Thread.sleep(2000);
+
+		// Save a screenshot of the page after scrolling.
 		TakesScreenshot tks = (TakesScreenshot)d;
 		File src=tks.getScreenshotAs(OutputType.FILE);
 		File dest=new File("./Ss/products-page.png");
 		FileHandler.copy(src, dest);
+
+		// Close the browser after the test is complete.
 		d.quit();
 	}
 }
